12d8a752f974f88ba2bc43abd3d7191e598b5af9
Marat <Shagiakhmetov2499@gmail.com>
2022-09-08 10:38:29 +0300

    Initial Commit

---
+++ Repetition_2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Repetition_2.3
+//
+//  Created by Marat Shagiakhmetov on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
